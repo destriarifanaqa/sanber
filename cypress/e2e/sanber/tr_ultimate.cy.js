b78386a4ab6957cd73b519a7c366786f0ac42f81
destriarifanaqa <destria.rifana.2103126@students.um.ac.id>
2025-11-02 17:21:05 +0700

    Delete cypress/e2e/sanber/tr_ultimate.cy.js
    file lain

--- cypress/e2e/sanber/tr_ultimate.cy.js
+++
@@ -1,6 +0,0 @@
-describe ('Dashboard UltimateQA', ()=>{
-    it('pilih select', ()=>{
-        cy.visit('https://ultimateqa.com/automation')
-        cy.get('[href="#"]').select('Free Courses')
-    })
-})
